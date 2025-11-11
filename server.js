2b096d402962d584ca3be88c9e5d005d8825415b
Luis <alphageekdom@gmail.com>
2025-11-11 10:23:00 -0800

    prepare for deployment

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ const PORT = process.env.PORT || 8000;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors());
+// CORS config
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
